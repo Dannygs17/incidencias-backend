6e9a6356bb10be3272725545b03b6d8af1b1f77b
Daniel <dannysantes17@gmail.com>
2026-04-09 09:55:11 -0600

    UI: Actualización de logo corporativo y limpieza de componentes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" href="{{ asset('images/loguito.png') }}" type="image/png">
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
         
